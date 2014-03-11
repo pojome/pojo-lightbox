f8b7b3d479c990c4111fb863e71357b8fcbe3363
Yakir Sitbon <kingyes1@gmail.com>
2014-03-11 14:25:52 +0000

    change links

--- classes/pojo-lightbox-admin-ui.php
+++ classes/pojo-lightbox-admin-ui.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 final class Pojo_Lightbox_Admin_UI {
 
 	public function plugin_action_links( $links ) {
-		$settings_link = sprintf( '<a href="%s" target="_blank">%s</a>', 'https://github.com/KingYes/pojo-lightbox', __( 'GitHub', 'pojo-lightbox' ) );
+		$settings_link = sprintf( '<a href="%s" target="_blank">%s</a>', 'https://github.com/pojome/pojo-lightbox', __( 'GitHub', 'pojo-lightbox' ) );
 		array_unshift( $links, $settings_link );
 
 		$settings_link = sprintf( '<a href="%s">%s</a>', admin_url( 'admin.php?page=pojo-lightbox' ), __( 'Settings', 'pojo-lightbox' ) );
--- pojo-lightbox.php
+++ pojo-lightbox.php
@@ -2,6 +2,7 @@
 /*
 Plugin Name: Pojo Lightbox
 Description: This plugin used to add the lightbox (overlay) effect to all images on your WordPress site.
+Plugin URI: http://pojo.me/
 Author: Pojo Team
 Version: 1.0.0
 Author URI: http://pojo.me/
